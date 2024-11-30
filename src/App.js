a17667ecbf0774e6f8676b7bcb4c4e9cdf4b77d5
KushagraSingh1100 <kushagrasingh1100@gmail.com>
2024-11-30 16:53:40 +0530

    Added Preloader

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
+import { RingLoader } from "react-spinners";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import DotRing from "./components/DotRing/DotRing";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
+import { useState, CSSProperties, useEffect } from "react";
 function App() {
+  const [loading, setloading] = useState(false);
+  useEffect(()=>{
+    setloading(true)
+    setTimeout(()=>{
+      setloading(false)
+    }, 5000)
+  }, [])
   return (
     <div className="App">
-      <DotRing />
+      { loading 
+      ? <div className="loader"><RingLoader
+        color={"#FFFFFF"}
+        loading={true}
+        cssOverride={false}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      <h1>Loading...</h1>
+      </div>
+      : <><DotRing />
       <Navbar />
       <Home />
       <About />
       <Projects />
       <Contact />
+      </>
+    }
     </div>
   );
 }
